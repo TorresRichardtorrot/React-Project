46985276e018594ebc2bccb21adf57ce386fb19d
TorresRichardtorrot <torresdrichar@gmail.com>
2023-11-23 21:05:33 -0400

    autenticacion

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -2,7 +2,8 @@ import { Navigate, Outlet } from 'react-router-dom'
 import { useAuth } from './hook/auth'
 
 export function ProtectedRoute () {
-  const { loading, isAuthenticated } = useAuth()
+  const { loading, isAuthenticated, user } = useAuth()
+  console.log(isAuthenticated, user)
 
   if (loading) return <h1>loading...</h1>
 
