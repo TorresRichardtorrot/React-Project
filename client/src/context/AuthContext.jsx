8244334ab5ba920d87f7cddd039448062cc13c8e
TorresRichardtorrot <torresdrichar@gmail.com>
2023-12-04 19:32:15 -0400

    change

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -47,7 +47,7 @@ export default function AuthProvider ({ children }) {
   useEffect(() => {
     async function checkLogin () {
       const cookies = Cookies.get()
-
+      console.log(cookies)
       if (!cookies.token) {
         setIsAuthenticated(false)
         setLoading(false)
